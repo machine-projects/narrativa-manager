87073acefbf8d1b85f99783be4711ba5825cc743
Felipe Matheus Toffoli Martins <felipe_toffoli1@hotmail.com>
2025-01-19 20:17:47 -0400

    no cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/:path*', // Aplica a todas as rotas e arquivos
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
+                    },
+                    {
+                        key: 'Surrogate-Control',
+                        value: 'no-store',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
